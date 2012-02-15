96126ccfa82754b5bcadc05eba88d0f904a36db8
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-15 22:20:46 +0000

    Implements XTeamSchedule::IO.read

--- lib/xteam_schedule/io.rb
+++ lib/xteam_schedule/io.rb
@@ -1,3 +1,9 @@
+require 'plist'
+
 class XTeamSchedule::IO
+  
+  def self.read(filename)
+    Plist.parse_xml(filename)
+  end
   
 end
--- spec/io_spec.rb
+++ spec/io_spec.rb
@@ -2,6 +2,22 @@ require 'spec_helper'
 
 describe XTeamSchedule::IO do
   
-  it 'should have some specs'
+  describe '.read' do
+    before do
+      @filename = 'path/to/file'
+    end
+    
+    it 'should use the plist gem to read from the file' do
+      Plist.should_receive(:parse_xml).with(@filename)
+      XTeamSchedule::IO.read(@filename)
+    end
+    
+    it 'should return a hash corresponding to the parsed file' do
+      stubbed_hash = { :foo => 'bar', :baz => 'quux' }
+      Plist.stub(:parse_xml).and_return(stubbed_hash)
+      return_value = XTeamSchedule::IO.read(@filename)
+      return_value.should == stubbed_hash
+    end
+  end
   
 end
