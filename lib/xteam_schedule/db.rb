7da58b5b445c8b59593668435222579b63c2078b
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-15 23:08:17 +0000

    Implements XTeamSchedule::DB.connect

--- lib/xteam_schedule/db.rb
+++ lib/xteam_schedule/db.rb
@@ -1,3 +1,12 @@
+require 'active_record'
+
 class XTeamSchedule::DB
+  
+  def self.connect
+    ActiveRecord::Base.establish_connection(
+      :adapter => 'sqlite3',
+      :database => ':memory:'
+    )
+  end
   
 end
--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -2,6 +2,16 @@ require 'spec_helper'
 
 describe XTeamSchedule::DB do
   
-  it 'has some specs'
+  describe '.connect' do
+    it "uses the 'sqlite3' adapter" do
+      ActiveRecord::Base.should_receive(:establish_connection).with(hash_including(:adapter => 'sqlite3'))
+      XTeamSchedule::DB.connect
+    end
+    
+    it "uses an 'in memory' database" do
+      ActiveRecord::Base.should_receive(:establish_connection).with(hash_including(:database => ':memory:'))
+      XTeamSchedule::DB.connect
+    end
+  end
   
 end
