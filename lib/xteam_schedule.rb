4b6e2f4806f2144f1c1aaa6e2293cfe9fcf27888
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-16 22:53:40 +0000

    Run DB.connect and DB.build_schema on initialisation

--- lib/xteam_schedule.rb
+++ lib/xteam_schedule.rb
@@ -3,3 +3,6 @@ require 'xteam_schedule/io'
 require 'xteam_schedule/db'
 require 'xteam_schedule/resource_group'
 require 'xteam_schedule/resource'
+
+XTeamSchedule::DB.connect
+XTeamSchedule::DB.build_schema
