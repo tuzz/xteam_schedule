23949ef8615e844a4c196270fcbd6941a890c8c9
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-19 12:51:17 +0000

    Implements XTeamSchedule::Composer#compose_resource_groups!

--- lib/xteam_schedule/facilitation/composer.rb
+++ lib/xteam_schedule/facilitation/composer.rb
@@ -12,8 +12,29 @@ class XTeamSchedule::Composer
   end
   
   def compose
-    #componentised compose methods
+    compose_resource_groups!
     hash
   end
+  
+private
+  
+  def compose_resource_groups!
+    hash['resource groups'] ||= []
+    schedule.resource_groups.each do |rg|
+      hash['resource groups'] << {
+        'name' => rg.name,
+        'expanded in library' => rg.expanded_in_library
+      }
+    end
+  end
+  
+  # def parse_resource_groups!
+  #   hash['resource groups'].try(:each) do |rg|
+  #     schedule.resource_groups.create!(
+  #       :name => rg['name'],
+  #       :expanded_in_library => rg['expanded in library']
+  #     )
+  #   end
+  # end
   
 end
--- spec/facilitation/composer_spec.rb
+++ spec/facilitation/composer_spec.rb
@@ -4,7 +4,8 @@ describe XTeamSchedule::Composer do
   
   describe '.compose' do
     it 'returns a hash' do
-      XTeamSchedule::Composer.compose({}).should be_a Hash
+      schedule = XTeamSchedule::Schedule.new
+      XTeamSchedule::Composer.compose(schedule).should be_a Hash
     end
   end
   
@@ -26,6 +27,36 @@ describe XTeamSchedule::Composer do
     it 'returns a hash' do
       @composer.compose.should be_a Hash
     end
+    
+    it 'calls compose_resource_groups!' do
+      @composer.should_receive(:compose_resource_groups!)
+      @composer.compose
+    end
   end
   
+  describe '#compose_resource_groups!' do
+    before do
+      @schedule = XTeamSchedule::Schedule.new
+      @schedule.resource_groups.new(:name => 'foo', :expanded_in_library => true)
+      @schedule.resource_groups.new(:name => 'bar', :expanded_in_library => false)
+      @composer = XTeamSchedule::Composer.new(@schedule)
+    end
+    
+    it 'creates resource groups' do
+      @composer.send(:compose_resource_groups!)
+      @composer.hash['resource groups'].count.should_not be_zero
+    end
+    
+    it 'sets the name key correctly' do
+      @composer.send(:compose_resource_groups!)
+      @composer.hash['resource groups'][0]['name'].should == 'foo'
+      @composer.hash['resource groups'][1]['name'].should == 'bar'
+    end
+    
+    it 'sets the expanded in library key correctly' do
+      @composer.send(:compose_resource_groups!)
+      @composer.hash['resource groups'][0]['expanded in library'].should be_true
+      @composer.hash['resource groups'][1]['expanded in library'].should be_false
+    end
+  end
 end
