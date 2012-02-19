5e159c68af76cc38497a4417a500ade36b4ae594
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-19 12:35:23 +0000

    Implements XTeamSchedule::Composer.compose

--- lib/xteam_schedule/facilitation/composer.rb
+++ lib/xteam_schedule/facilitation/composer.rb
@@ -1,3 +1,19 @@
 class XTeamSchedule::Composer
-
+  
+  attr_accessor :schedule, :hash
+  
+  def self.compose(schedule)
+    new(schedule).compose
+  end
+  
+  def initialize(schedule)
+    self.schedule = schedule
+    self.hash = {}
+  end
+  
+  def compose
+    #componentised compose methods
+    hash
+  end
+  
 end
--- spec/facilitation/composer_spec.rb
+++ spec/facilitation/composer_spec.rb
@@ -2,6 +2,30 @@ require 'spec_helper'
 
 describe XTeamSchedule::Composer do
   
-  it 'has some specs'
+  describe '.compose' do
+    it 'returns a hash' do
+      XTeamSchedule::Composer.compose({}).should be_a Hash
+    end
+  end
+  
+  describe '#initialize' do
+    it 'sets the schedule and hash instance variables' do
+      schedule = XTeamSchedule::Schedule.new
+      composer = XTeamSchedule::Composer.new(schedule)
+      composer.schedule.should == schedule
+      composer.hash.should_not be_nil
+    end
+  end
+  
+  describe '#compose' do
+    before do
+      schedule = XTeamSchedule::Schedule.new
+      @composer = XTeamSchedule::Composer.new(schedule)
+    end
+    
+    it 'returns a hash' do
+      @composer.compose.should be_a Hash
+    end
+  end
   
 end
