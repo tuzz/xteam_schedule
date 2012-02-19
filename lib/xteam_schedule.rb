ea8529b4040021dfda9f23d89d37d940dba8c283
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-19 19:18:08 +0000

    Remove leading 'lib/' and trailing '.rb' to respect the load path

--- lib/xteam_schedule.rb
+++ lib/xteam_schedule.rb
@@ -3,7 +3,7 @@ require 'active_record'
 require 'sqlite3'
 require 'xteam_schedule/core'
 
-Dir['lib/**/*.*'].each { |f| require f }
+Dir['lib/**/*.*'].each { |f| require f[4..-4] }
 
 XTeamSchedule::DB.connect
 XTeamSchedule::DB.build_schema
