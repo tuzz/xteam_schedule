f32caae92b3a18187b8151951dc91510e48a5840
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-21 21:22:40 +0000

    Adds the 'colour' attribute to Assignment

--- lib/xteam_schedule/models/assignment.rb
+++ lib/xteam_schedule/models/assignment.rb
@@ -5,5 +5,17 @@ class XTeamSchedule::Assignment < ActiveRecord::Base
   
   validates :name, :presence => true,
                    :uniqueness => true
-  validates_presence_of :kind
+  validates_presence_of :kind, :colour
+  validate :rgba_colour
+  
+private
+
+  def rgba_colour
+    components = self[:colour].split(',').map { |c| Float(c) }
+    raise 'invalid' if components.any?(&:nil?) or components.count != 4
+    raise 'invalid' if components.any? { |c| c < 0 || c > 1 }
+  rescue
+    errors.add(:colour, 'does match the RGBA format')
+  end
+  
 end
