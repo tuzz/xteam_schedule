45ac45a123f8d58ecc5356f282e11792ccc890c8
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-26 23:53:04 +0000

    Require all of the files

--- lib/xteam_schedule.rb
+++ lib/xteam_schedule.rb
@@ -3,7 +3,20 @@ require 'active_record'
 require 'sqlite3'
 require 'xteam_schedule/core'
 
-Dir['lib/**/*.*'].each { |f| require f[4..-4] }
+require 'xteam_schedule/models/assignment'
+require 'xteam_schedule/models/assignment_group'
+require 'xteam_schedule/models/interface'
+require 'xteam_schedule/models/resource'
+require 'xteam_schedule/models/resource_group'
+require 'xteam_schedule/models/schedule'
+require 'xteam_schedule/models/weekly_working_schedule'
+require 'xteam_schedule/models/working_day'
+require 'xteam_schedule/models/working_time'
+
+require 'xteam_schedule/facilitation/composer'
+require 'xteam_schedule/facilitation/db'
+require 'xteam_schedule/facilitation/io'
+require 'xteam_schedule/facilitation/parser'
 
 XTeamSchedule::DB.connect
 XTeamSchedule::DB.build_schema
