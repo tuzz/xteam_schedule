41aa871f54ebfc6d543ef6d7ee4eb0555f2c015d
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-02-26 19:40:16 +0000

    Implements the top level proxy with a helpful inspect method

--- lib/xteam_schedule/core.rb
+++ lib/xteam_schedule/core.rb
@@ -1,3 +1,26 @@
 class XTeamSchedule
-
+  
+  def initialize(filename = nil)
+    if filename.present?
+      begin
+        hash = IO.read(filename)
+        @schedule = Parser.parse(hash)
+      rescue
+        raise 'Malformed xTeam Schedule file'
+      end
+    else
+      @schedule = Schedule.new
+    end
+  end
+  
+  def method_missing(*args);
+    @schedule.send(*args)
+  end
+  
+  def inspect
+    stats = [:resource_groups, :resources, :assignment_groups, :assignments, :working_times].map { |s| [s, send(s).count] }
+    stats_string = stats.map { |s| "#{s.first}(#{s.second})" }.join(', ')
+    "#<XTeamSchedule #{stats_string}>"
+  end
+  
 end
--- spec/core_spec.rb
+++ spec/core_spec.rb
@@ -2,6 +2,26 @@ require 'spec_helper'
 
 describe XTeamSchedule do
   
-  it 'has some specs'
+  describe '#initialize' do
+    before do
+      XTeamSchedule::IO.stub!(:read).and_return({ 'resource groups' => [
+        { 'expandedInLibrary' => true, 'name' => 'foo'}
+      ]})
+    end
+    
+    it 'can be initialized with no parameters' do
+      lambda {
+        schedule = XTeamSchedule.new
+        schedule.resource_groups.should be_empty
+      }.should_not raise_error
+    end
+    
+    it 'can be initialized by passing in a filename' do
+      lambda {
+        schedule = XTeamSchedule.new('path/to/file')
+        schedule.resource_groups.should_not be_empty
+      }.should_not raise_error
+    end
+  end
   
 end
