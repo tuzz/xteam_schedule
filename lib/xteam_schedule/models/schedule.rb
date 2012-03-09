7b991856749979778ba80738c9af9c813f7e1935
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-03-09 21:14:24 +0000

    No has_many through through

--- lib/xteam_schedule/models/schedule.rb
+++ lib/xteam_schedule/models/schedule.rb
@@ -3,7 +3,6 @@ class XTeamSchedule::Schedule < ActiveRecord::Base
   has_many :resources, :through => :resource_groups
   has_many :assignment_groups, :dependent => :destroy
   has_many :assignments, :through => :assignment_groups
-  has_many :working_times, :through => :resources
   has_many :working_days, :through => :weekly_working_schedule
   
   has_one :interface
@@ -12,6 +11,15 @@ class XTeamSchedule::Schedule < ActiveRecord::Base
   def after_initialize
     set_default_interface
     set_default_weekly_working_schedule
+  end
+  
+  def working_times
+    XTeamSchedule::WorkingTime.find_by_sql([
+    "select distinct * from working_times as wt
+     join resources r on wt.resource_id = r.id
+     join resource_groups rg on r.resource_group_id = rg.id
+     where rg.schedule_id = ?", id
+    ])
   end
   
 private
