211e70529a47b3ba6cf65040a29909b297ff3603
Christopher Patuzzo <chris.patuzzo@gmail.com>
2012-04-14 18:07:19 +0100

    Adds a remote access validation for name

--- lib/xteam_schedule/models/remote_access.rb
+++ lib/xteam_schedule/models/remote_access.rb
@@ -1,3 +1,20 @@
 class XTeamSchedule::RemoteAccess < XTeamSchedule::Base
   belongs_to :schedule
+
+  validate :format_of_name
+  NAME_REGEX = /XTEAM-\d{8}-\d{4}/.freeze
+
+private
+
+  def format_of_name
+    return unless name
+    raise 'invalid' unless name =~ NAME_REGEX
+    xteam, date, time = name.split('-')
+    day, month, year = date[0..1], date[2..3], date[4..7]
+    hour, minute = time[0..1], time[2..3]
+    DateTime.new(year.to_i, month.to_i, day.to_i, hour.to_i, minute.to_i)
+  rescue
+    errors.add(:name, 'is not in the form XTEAM-DDMMYYYY-HHMM')
+  end
+
 end
--- spec/models/remote_access_spec.rb
+++ spec/models/remote_access_spec.rb
@@ -14,4 +14,24 @@ describe XTeamSchedule::RemoteAccess do
     end
   end
 
+  describe 'validations' do
+    before do
+      @remote_access = Factory(:remote_access)
+    end
+
+    it 'requires a name in format XTEAM-DDMMYYYY-HHMM' do
+      @remote_access.server_id = 1
+
+      %w(XTEAM-01012000-0000 XTEAM-31122012-2359 XTEAM-05052005-0505).each do |valid|
+        @remote_access.name = valid
+        @remote_access.should be_valid, valid
+      end
+
+      %w(invalid XTEAM-01234567-0123 XTEAM-31022012-1200 XTEAM-01012012-2401).each do |invalid|
+        @remote_access.name = invalid
+        @remote_access.should_not be_valid, invalid
+      end
+    end
+  end
+
 end
